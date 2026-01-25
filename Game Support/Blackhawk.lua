4b189b5dcb4db5efe9a6721ad88b7bee23da3ccf
region3 <126637834+Jimenth@users.noreply.github.com>
2026-01-25 07:07:50 -0500

    Update Blackhawk.lua

--- Game Support/Blackhawk.lua
+++ Game Support/Blackhawk.lua
@@ -1,6 +1,6 @@
 local Added = {}
 local Workspace = game:GetService("Workspace")
-local ClosestPlayer
+local Closest
 local PlaceID = game.PlaceId
 
 local Places = {
@@ -12,7 +12,6 @@ local Places = {
 }
 
 local function IsPlayerModel(Model)
-    if PlaceID == Places["2V2"] or PlaceID == Places["5V5"] or PlaceID == Places["Ranked"] then return false end
     return Model:FindFirstChild("BillboardGui") ~= nil
 end
 
@@ -24,23 +23,34 @@ local function GetClosestPlayer()
     local Camera = Workspace:FindFirstChild("Camera")
     if not Camera then return nil end
 
-    local Closest = nil
+    if Camera:FindFirstChild("WorldModel") then return nil end
+
+    local ClosestModel = nil
     local ClosestDistance = math.huge
+    local CameraPosition = Camera.Position
 
     for _, Model in pairs(Workspace:GetChildren()) do
         if Model.ClassName == "Model" and Model.Name == "Male" and IsPlayerModel(Model) then
-            local HumanoidRootPart = Model:FindFirstChild("HumanoidRootPart")
-            if HumanoidRootPart then
-                local Distance = vector.magnitude(HumanoidRootPart.Position - Camera.Position)
+            local Root = Model:FindFirstChild("Root")
+            if Root then
+                local Distance = vector.magnitude(Root.Position - CameraPosition)
                 if Distance < ClosestDistance then
                     ClosestDistance = Distance
-                    Closest = Model
+                    ClosestModel = Model
                 end
             end
         end
     end
 
-    return Closest
+    return ClosestModel
+end
+
+local function CheckWorldModel()
+    local Camera = Workspace:FindFirstChild("Camera")
+    if not Camera then return false end
+    
+    local WorldModel = Camera:FindFirstChild("WorldModel")
+    return WorldModel ~= nil
 end
 
 local function GetBodyParts(Model)
@@ -65,7 +75,7 @@ local function GetBodyParts(Model)
         RightLowerLeg = Model:FindFirstChild("RightLowerLeg"),
         RightFoot = Model:FindFirstChild("RightFoot"),
 
-        HumanoidRootPart = Model:FindFirstChild("HumanoidRootPart"),
+        HumanoidRootPart = Model:FindFirstChild("Root"),
     }
 end
 
@@ -99,10 +109,10 @@ local function PlayerData(Model, Parts)
         RightFoot = Parts.RightFoot,
         BodyHeightScale = 1,
         RigType = 1,
-        Toolname = "Unknown",
+        Toolname = "None",
         Teamname = IsPlayerModel(Model) and "Players" or IsZombieModel(Model) and "Zombies" or "NPCs",
         Whitelisted = false,
-        Archenemies = false,
+        Archenemies = IsPlayerModel(Model) and true or false,
         Aimbot_Part = Parts.Head,
         Aimbot_TP_Part = Parts.Head,
         Triggerbot_Part = Parts.Head,
@@ -148,8 +158,28 @@ end
 
 task.spawn(function()
     while true do
-        task.wait(1)
-        ClosestPlayer = GetClosestPlayer()
+        task.wait(0.5)
+        local New = GetClosestPlayer()
+        
+        if PlaceID == Places["Openworld"] then
+            State = false
+            Excluded = nil
+            Closest = nil
+        elseif PlaceID == Places["2V2"] or PlaceID == Places["5V5"] or PlaceID == Places["Ranked"] then
+            State = false
+            Excluded = nil
+            Closest = New
+        else
+            if CheckWorldModel() then
+                State = false
+                Excluded = nil
+            else
+                State = true
+                Excluded = New
+            end
+            
+            Closest = New
+        end
     end
 end)
 
@@ -164,22 +194,24 @@ local function Update()
                 local Key = tostring(Object)
                 if not Key then return end
                 
+                if PlaceID == Places["Openworld"] and Object.Name == "Male" and IsPlayerModel(Object) then
+                    return
+                end
+                
                 local Parts = GetBodyParts(Object)
 
                 if Parts and Parts.Head and Parts.HumanoidRootPart then
-                    if PlaceID ~= Places["Zombies"] and ClosestPlayer and Object == ClosestPlayer then return end
-
                     if not Added[Key] then
                         local Success2, ID, Data = pcall(function()
                             return PlayerData(Object, Parts)
                         end)
                         
                         if Success2 and ID and Data then
-                            local Success3, result = pcall(function()
+                            local Success3, Result = pcall(function()
                                 return add_model_data(Data, ID)
                             end)
                             
-                            if Success3 and result then
+                            if Success3 and Result then
                                 Added[ID] = Object
                             end
                         end
@@ -199,7 +231,9 @@ local function Update()
                 return
             end
             
-            local Success, HumanoidRootPart = pcall(function() return Model:FindFirstChild("HumanoidRootPart") end)
+            local Success, HumanoidRootPart = pcall(function() 
+                return Model:FindFirstChild("Root") 
+            end)
             
             if not Success then HumanoidRootPart = nil end
             
@@ -216,7 +250,108 @@ local function LocalPlayerData()
     local Camera = Workspace:FindFirstChild("Camera")
     if not Camera then return end
 
-    local LocalData = {
+    if PlaceID == Places["Openworld"] then
+        local Data = {
+            LocalPlayer = Camera,
+            Character = Camera,
+            Username = tostring(Camera),
+            Displayname = game.Players.LocalPlayer.Name,
+            Userid = 1,
+            Team = Camera,
+            Tool = Camera,
+            Humanoid = Camera,
+            Health = 100,
+            MaxHealth = 100,
+            RigType = 1,
+
+            Head = Camera,
+            RootPart = Camera,
+            LeftFoot = Camera,
+            LowerTorso = Camera,
+        }
+
+        override_local_data(Data)
+        return
+    end
+
+    if Closest then
+        local Parts = GetBodyParts(Closest)
+        if Parts and Parts.Head and Parts.HumanoidRootPart then
+            local Data = {
+                LocalPlayer = Closest,
+                Character = Closest,
+                Username = tostring(Closest),
+                Displayname = game.Players.LocalPlayer.Name,
+                Userid = 1,
+                Teamname = IsPlayerModel(Closest) and "Players" or IsZombieModel(Closest) and "Zombies" or "NPCs",
+                Toolname = "None",
+                Humanoid = Parts.Head,
+                Health = 100,
+                MaxHealth = 100,
+                RigType = 1,
+
+                RootPart = Parts.HumanoidRootPart,
+                Head = Parts.Head,
+                Torso = Parts.UpperTorso,
+                UpperTorso = Parts.UpperTorso,
+                LowerTorso = Parts.LowerTorso,
+                LeftArm = Parts.LeftUpperArm,
+                LeftLeg = Parts.LeftUpperLeg,
+                RightArm = Parts.RightUpperArm,
+                RightLeg = Parts.RightUpperLeg,
+                LeftUpperArm = Parts.LeftUpperArm,
+                LeftLowerArm = Parts.LeftLowerArm,
+                LeftHand = Parts.LeftHand,
+                RightUpperArm = Parts.RightUpperArm,
+                RightLowerArm = Parts.RightLowerArm,
+                RightHand = Parts.RightHand,
+                LeftUpperLeg = Parts.LeftUpperLeg,
+                LeftLowerLeg = Parts.LeftLowerLeg,
+                LeftFoot = Parts.LeftFoot,
+                RightUpperLeg = Parts.RightUpperLeg,
+                RightLowerLeg = Parts.RightLowerLeg,
+                RightFoot = Parts.RightFoot,
+                body_parts_data = {
+                    { name = "LowerTorso", part = Parts.LowerTorso },
+                    { name = "LeftUpperLeg", part = Parts.LeftUpperLeg },
+                    { name = "LeftLowerLeg", part = Parts.LeftLowerLeg },
+                    { name = "RightUpperLeg", part = Parts.RightUpperLeg },
+                    { name = "RightLowerLeg", part = Parts.RightLowerLeg },
+                    { name = "LeftUpperArm", part = Parts.LeftUpperArm },
+                    { name = "LeftLowerArm", part = Parts.LeftLowerArm },
+                    { name = "RightUpperArm", part = Parts.RightUpperArm },
+                    { name = "RightLowerArm", part = Parts.RightLowerArm },
+                },
+                full_body_data = {
+                    { name = "Head", part = Parts.Head },
+                    { name = "UpperTorso", part = Parts.UpperTorso },
+                    { name = "LowerTorso", part = Parts.LowerTorso },
+                    { name = "HumanoidRootPart", part = Parts.HumanoidRootPart },
+
+                    { name = "LeftUpperArm", part = Parts.LeftUpperArm },
+                    { name = "LeftLowerArm", part = Parts.LeftLowerArm },
+                    { name = "LeftHand", part = Parts.LeftHand },
+
+                    { name = "RightUpperArm", part = Parts.RightUpperArm },
+                    { name = "RightLowerArm", part = Parts.RightLowerArm },
+                    { name = "RightHand", part = Parts.RightHand },
+
+                    { name = "LeftUpperLeg", part = Parts.LeftUpperLeg },
+                    { name = "LeftLowerLeg", part = Parts.LeftLowerLeg },
+                    { name = "LeftFoot", part = Parts.LeftFoot },
+
+                    { name = "RightUpperLeg", part = Parts.RightUpperLeg },
+                    { name = "RightLowerLeg", part = Parts.RightLowerLeg },
+                    { name = "RightFoot", part = Parts.RightFoot },
+                }
+            }
+
+            override_local_data(Data)
+            return
+        end
+    end
+
+    local Data = {
         LocalPlayer = Camera,
         Character = Camera,
         Username = tostring(Camera),
@@ -235,14 +370,12 @@ local function LocalPlayerData()
         LowerTorso = Camera,
     }
 
-    override_local_data(LocalData)
+    override_local_data(Data)
 end
 
 RunService.PostLocal:Connect(function()
     Update()
-
-    local LocalID, LocalData = LocalPlayerData()
-    if LocalID and LocalData then
-        override_local_data(LocalData)
-    end
+    
+    local ID, Data = LocalPlayerData()
+    if ID and Data then override_local_data(Data) end
 end)
